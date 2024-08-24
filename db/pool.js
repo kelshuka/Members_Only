930f8a54166d31851cca630109d1488dcd09314c
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-08-24 18:06:10 +0000

    pool

--- db/pool.js
+++ db/pool.js
@@ -2,25 +2,18 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 
+
+const pool = new Pool({
+    connectionString : process.env.DATABASE_URL,
+   
+});
+
+module.exports = pool;
+
 // original for my local
-module.exports = new Pool({
+/* module.exports = new Pool({
     host: "localhost", 
     user: process.env.ROLE_NAME,
     database: process.env.DATABASE,
     password: process.env.PASSWORD
-});
-
-/* const isProduction = process.env.NODE_ENV === "production";
-
-const connectionString = 
-`postgresql://${process.env.ROLE_NAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
-
-
-const pool = new Pool({
-    connectionString: isProduction ?  process.env.DATABASE_URL : connectionString ,
-   /*  max: 5, //Maximum number of clients in the pool
-    idleTimeoutMillis: 30000, // 30 seconds
-    connectionTimeoutMillis: 5000, // 5 seconds 
-});
-
-module.exports = pool; */
+}); */
